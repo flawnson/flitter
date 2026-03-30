1f71051f4664eecf50d72e5cd336a1ab58ec760b
flawnson <flawnsontong1@gmail.com>
2026-03-30 16:20:58 -0400

    Initial Commit

---
+++ flitter/flitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  flitterApp.swift
+//  flitter
+//
+//  Created by Flawnson Tong on 2026-03-30.
+//
+
+import SwiftUI
+
+@main
+struct flitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
