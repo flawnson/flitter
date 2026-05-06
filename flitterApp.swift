aaff9cd71234cc6b1f1ec785c56c2efbc5a6d85a
flawnson <flawnsontong1@gmail.com>
2026-05-06 19:50:53 -0400

    Merging?

---
+++ flitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MicroblogApp.swift
+//  flitter
+//
+//  Created by Flawnson Tong on 2026-03-30.
+//
+
+import SwiftUI
+
+@main
+struct MicroblogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            FeedView()
+        }
+    }
+}
